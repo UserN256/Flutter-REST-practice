b7c763c9e916702f8b1a23e1b152c0aa1f3e0700
UserN256 <MoyAkkDlyaCoding@gmail.com>
2024-05-14 23:15:09 +0300

    Yes! Athentication works!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
-// 1st variant commented below. Originalnoe zakommentil v konce
-import 'dart:async';
+// 1st 3 variants commented below. Originalnoe zakommentil v konce
+// Copyright (c) 2018, Marco Esposito (marcoesposito1988@gmail.com).
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+import 'package:http_auth/http_auth.dart';
+import 'dart:io';
+
+void main() {
+  var client = BasicAuthClient('admin@gmail.com', 'admin');
+
+  final url = Uri.parse('https://javaops-demo.ru/topjava/rest/admin/users');
+
+  client.get(url).then((r) => stderr.writeln((r.body)));
+}
+
+/*import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -224,4 +240,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-*/
+*/*/
